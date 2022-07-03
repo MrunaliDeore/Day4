79583d67c1cddbe72e9aab1cfcfb84af8298570d
MrunaliDeore <mrunalideoreohk1@gmail.com>
2022-07-03 18:30:16 +0530

    Added: UC2 snake ladder

---
+++ SnakeLadderUC2.java
@@ -0,0 +1,11 @@
+package Four;
+
+public class SnakeLadderUC2
+{	
+	public static void main(String[] args)
+	{
+		int die1;   // player die    
+	    die1 = (int)(Math.random()*6) + 1;    
+	    System.out.println("The player die comes up " + die1);    
+	}
+}
